5c7b8cf0bd67e8a761e87412c66cb6f0aa2ca781
jiseop9083 <sinphi03@gmail.com>
2024-07-31 15:20:31 +0900

    feat: api test

--- front/src/pages/files/Files.tsx
+++ front/src/pages/files/Files.tsx
@@ -1,12 +1,18 @@
+import { getBubble } from '@/api/bubble';
 import style from '@/pages/files/files.module.css';
 import { FreeView } from '@/pages/files/FreeView';
 import { GridView } from '@/pages/files/GridView';
 
 export const Files = () => {
+    const apiHandler = async () => {
+        const bubble = await getBubble('Workspace1', '/ws1');
+        console.log('bbule', bubble);
+    };
     return (
         <div className={style.default}>
-            {/* <GridView /> */}
-            <FreeView />
+            <GridView />
+            {/* <FreeView /> */}
+            <button onClick={apiHandler}>API 호출</button>
         </div>
     );
 };
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,6 +5,18 @@ import postcssNesting from 'postcss-nesting';
 
 export default defineConfig({
     plugins: [react()],
+    // server: {
+    //     proxy: {
+    //         '/api': {
+    //             target: 'http://localhost:8080',
+    //             changeOrigin: true,
+    //             secure: false,
+    //             headers: {
+    //                 Origin: 'http://localhost:8080',
+    //             },
+    //         },
+    //     },
+    // },
     resolve: {
         alias: {
             '@': '/src',
