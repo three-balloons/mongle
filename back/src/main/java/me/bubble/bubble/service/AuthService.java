efd1ba642253ce68cd6291e2f660ce1f9b5ef3c9
LeeJinHyeok <kmer1024@hanyang.ac.kr>
2024-08-19 16:05:44 +0900

    feat/카카오 로그인 구현 끝

--- back/src/main/java/me/bubble/bubble/service/AuthService.java
+++ back/src/main/java/me/bubble/bubble/service/AuthService.java
@@ -32,15 +32,15 @@ public class AuthService {
         Mono<KakaoResponseDto> kakaoResponseDtoMono = webClient.post()
                 .uri("https://kauth.kakao.com/oauth/token")
                 .header("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
-                .bodyValue("grant_type=authorization_code&client_id="+clientId+"&redirect_uri="+redirectUri+"&code=" + code)
+                .bodyValue("grant_type=authorization_code&client_id="+clientId+"&redirect_uri="+redirectUri+"&code=" + code+"&client_secret="+clientSecret)
                 .retrieve()
                 .bodyToMono(KakaoResponseDto.class);
         //요청을 받아서, idToken을 decode 후에 sub을 가져온다.sub: 유저 식별자
         String idToken = kakaoResponseDtoMono.block().getId_token();
         final String payloadJWT = idToken.split("\\.")[1];
-        String payload = Arrays.toString(decoder.decode(payloadJWT));
-
-        Map<String, Object> jsonArray = jsonParser.parseMap(payload);
+        byte[] payload = decoder.decode(payloadJWT);
+        String decodedPayload = new String(payload);
+        Map<String, Object> jsonArray = jsonParser.parseMap(decodedPayload);
         if (!jsonArray.containsKey("sub")) {
             return ""; //Exception Handling
         }
